954ce406ceb53dd252db4ecc60f7f745acc02375
acgraczyk <a.c.graczyk@gmail.com>
2020-06-01 13:28:37 -0400

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -3,14 +3,17 @@ from django.http import HttpResponse
 
 from .models import Greeting
 import requests
+import os
 
 
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
-    r=requests.get('http://httpbin.org/status/418')
-    print(r.text)
-    return HttpResponse('<pre>'+r.text+'</pre>')
+    #r=requests.get('http://httpbin.org/status/418')
+   # print(r.text)
+   # return HttpResponse('<pre>'+r.text+'</pre>')
+   times = int(os.environment.get('TIMES',3))
+   return HttpResponse('Hello' * times)
 
 
 def db(request):
